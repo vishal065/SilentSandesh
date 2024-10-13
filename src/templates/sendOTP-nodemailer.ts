dff6002bf1f1d2475b3c532b4c44fb9b56120ca2
vishal <dasvishal60@gmail.com>
2024-10-13 18:14:09 +0530

    path fix to ejs

--- src/templates/sendOTP-nodemailer.ts
+++ src/templates/sendOTP-nodemailer.ts
@@ -16,7 +16,7 @@ export async function SendMailTemplate(email: string, template: templateProp) {
   try {
     const mailtransporter = mailer.createTransport({
       service: "gmail",
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.AUTH_MAIL,
         pass: process.env.AUTH_PASS,
@@ -27,41 +27,31 @@ export async function SendMailTemplate(email: string, template: templateProp) {
     });
 
     // Recreate __dirname behavior in ES module
-    const __filename = fileURLToPath(import.meta.url);
-    console.log("__filename is ", __filename);
+    // const __filename = fileURLToPath(import.meta.url);
 
-    const __dirname = path.dirname(__filename);
-    console.log("__dirname is ", __dirname);
+    // const __dirname = path.dirname(__filename);
 
-    const templatePath = path.join(__dirname, "src", "templates", template.url); //(__dirname,"inner folder if any", template.url);
-    console.log("template object", template);
-    console.log("templatePath is ", templatePath);
+    //! Note templatePath is not working in production;
+    // const templatePath = path.join(__dirname, "src", "templates", template.url); //(__dirname,"inner folder if any", template.url);
 
     const absoulutePath = path.join(
       process.cwd(),
       "src",
       "templates",
-      "sendOTP-nodemailer.ejs"
+      template.url
     );
     console.log("absoulte path ", absoulutePath);
 
-    // const templatesDir = path.join(__dirname, "src", "templates");
-
-    // console.log("Templates directory:", fs.readdirSync(templatesDir));
-
     const templatefile = fs.readFileSync(absoulutePath, "utf-8");
-    console.log("templatefile is ", templatefile);
 
     const html = ejs.render(templatefile, template);
-    console.log("html is ", html);
 
     const mailingdetail = {
       from: process.env.AUTH_MAIL,
       to: email,
-      subject: " Verification Code ",
+      subject: "Verification Code",
       html,
     };
-    console.log("mailingdetail is ", mailingdetail);
 
     const resp = await mailtransporter.sendMail(mailingdetail);
     console.log("resp is ", resp);
