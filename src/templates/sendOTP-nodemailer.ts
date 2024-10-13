33492f890a61b04d0ec98a3049e70a4e01ff6cb3
vishal <dasvishal60@gmail.com>
2024-10-13 17:16:55 +0530

    checking otp

--- src/templates/sendOTP-nodemailer.ts
+++ src/templates/sendOTP-nodemailer.ts
@@ -28,22 +28,31 @@ export async function SendMailTemplate(email: string, template: templateProp) {
 
     // Recreate __dirname behavior in ES module
     const __filename = fileURLToPath(import.meta.url);
+    console.log("__filename is ", __filename);
+
     const __dirname = path.dirname(__filename);
+    console.log("__dirname is ", __dirname);
 
     const templatePath = path.join(__dirname, template.url); //(__dirname,"inner folder if any", template.url);
+    console.log("templatePath is ", templatePath);
 
     const templatefile = fs.readFileSync(templatePath, "utf-8");
+    console.log("templatefile is ", templatefile);
 
     const html = ejs.render(templatefile, template);
+    console.log("html is ", html);
 
-    const malingdetail = {
+    const mailingdetail = {
       from: process.env.AUTH_MAIL,
       to: email,
       subject: " Verification Code ",
       html,
     };
+    console.log("mailingdetail is ", mailingdetail);
 
-    const resp = await mailtransporter.sendMail(malingdetail);
+    const resp = await mailtransporter.sendMail(mailingdetail);
+    console.log("resp is ", resp);
+
     return resp;
   } catch (error) {
     console.error("Error sending email:", error);
